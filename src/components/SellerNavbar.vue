8012ce9955528ba18715875d40157c7905fd78d3
vernecic <vidernecic1@gmail.com>
2025-05-04 20:39:10 +0200

    go back

--- src/components/SellerNavbar.vue
+++ src/components/SellerNavbar.vue
@@ -15,25 +15,25 @@
         class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
       >
         <li>
-          <router-view
-            to="my-listings"
+          <a
+            href="#"
             class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
             aria-current="page"
-            >Home</router-view
+            >Home</a
           >
         </li>
         <li>
-          <router-link
-            to="/seller-orders"
+          <a
+            href="#"
             class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >Orders</router-link
+            >Orders</a
           >
         </li>
         <li>
-          <router-link
-            to="/notifications"
+          <a
+            href="#"
             class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >Notifications</router-link
+            >Notifications</a
           >
         </li>
 
